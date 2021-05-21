4810a9a6a11c68d364d1678aeb67a3556f79012a
gordusha1311 <82509731+gordusha1311@users.noreply.github.com>
2021-05-21 18:47:19 +0700

    Добавили слои

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let cnv = document.getElementById("canvas");
+let cnv_bg = document.getElementById("bg_layer");
 let ctx = cnv.getContext("2d");
-let color = document.getElementById("color").value
+let ctx_bg = cnv_bg.getContext("2d");
+let color = document.getElementById("color").value;
 let background = document.getElementById("background");
 let width = 30;
 let tool = "Кисточка";
-let button = document.getElementById("tool");
+let button_tool = document.getElementById("tool");
+let button_clear = document.getElementById("clear");
 
 let displayWidth = cnv.clientWidth;
 let displayHeight = cnv.clientHeight;
 cnv.width = displayWidth;
 cnv.height = displayHeight;
-ctx.fillStyle = background.value;
-ctx.fillRect(0, 0, cnv.width, cnv.height);
+cnv_bg.width = displayWidth;
+cnv_bg.height = displayHeight;
+ctx_bg.fillStyle = background.value;
+ctx_bg.fillRect(0, 0, cnv_bg.width, cnv_bg.height);
 
 document.getElementById("color").oninput = function(){
     color = this.value;
@@ -22,6 +27,10 @@ document.getElementById("size").oninput = function(){
 }
 
 document.getElementById("save_image").onclick = function(){
+    let cnv_save = document.getElementById("save_canvas");
+    let ctx_save = cnv_save.getContext("2d");
+    ctx_save.drawImage(ctx_bg, 0, 0);
+    ctx_save.drawImage(ctx, 0, 0);
     let image = cnv.toDataURL("image/jpg");
     this.href = image;
 }
@@ -37,9 +46,9 @@ cnv.onmousedown = (e) => {
         };
     }, 1);
     if(tool == "Кисточка")
-    ctx.fillStyle = color;
-else
-    ctx.fillStyle = background.value;
+                ctx.fillStyle = color;
+            else
+                ctx.fillStyle = background.value;
     ctx.fillRect(e.offsetX - width/2, e.offsetY- width/2, width, width);
     cnv.onmouseup = () => {
         cnv.onmousemove = null;
@@ -49,20 +58,28 @@ else
 background.addEventListener('input', changeBackground);
 
 function changeBackground(){
-    ctx.fillStyle = background.value;
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
+    ctx_bg.fillStyle = background.value;
+    ctx_bg.fillRect(0, 0, cnv_bg.width, cnv_bg.height);
 }
 
-button.addEventListener("click", changeTool);
+button_tool.addEventListener("click", changeTool);
 
 function changeTool(){
     let btn = document.getElementById("tool");
-    if(btn.textContext == "Кисточка"){
-        btn.textContext = "Ластик";
+    if(btn.textContent == "Кисточка"){
+        btn.textContent = "Ластик";
         tool = "Ластик";
     }
     else{
-        btn.textContext = "Кисточка";
+        btn.textContent = "Кисточка";
         tool = "Кисточка";
     }
+}
+
+button_clear.addEventListener("click", clearCanvas);
+
+function clearCanvas(){
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    ctx.fillStyle = background.value;
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
 }
